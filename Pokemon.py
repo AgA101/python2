bdf402f139342169e22e61656a2ea662bfcc018d
George <alekseevg888@gmail.com>
2021-12-04 20:03:27 +0300

    аннотации + доп задание 2

--- Pokemon.py
+++ Pokemon.py
@@ -1,12 +1,24 @@
 import requests
+from collections.abc import Iterator
+
+
 class BasePokemon:
-    def __init__(self,name):
-        self.name = str(name)
+    name: str
+
+    def __init__(self,name: str) -> None:
+        self.name = name
 
     def __str__(self):
         return f'name={self.name}'
+
+
 class Pokemon(BasePokemon):
-    def __init__(self, name, id, height, weight):
+    name: str
+    id: str
+    height: str
+    weight: str
+
+    def __init__(self, name: str, id: str, height: str, weight: str) -> None:
         BasePokemon.__init__(self, name)
         self.__name = self.name
         self.__id = id
@@ -32,14 +44,25 @@ class Pokemon(BasePokemon):
     def __str__(self):
         return f'name={self.__name}, id={self.__id}, height={self.__height}, weight={self.__weight}'
 
-class PokeAPI:
-    def get_pokemon(self,name_id):
-        url = "https://pokeapi.co/api/v2/pokemon/" + name_id + "/"
-        result = requests.get(url).json()
-        return Pokemon(result["name"], result["id"], result["height"], result["weight"])
 
-    def get_all(self, max_counter, get_full=False):
-        counter = 1
+class PokeAPI:
+    name_id: str
+    max_counter: int
+
+    def get_pokemon(self, name_id: str) -> Pokemon:
+        for i in range(len(pokeball)):
+            if pokeball[i].name == name_id or pokeball[i].id == name_id:
+                print("ofghh")
+                return pokeball[i]
+        else:
+            url = "https://pokeapi.co/api/v2/pokemon/" + name_id + "/"
+            result = requests.get(url).json()
+            pokeball.append(Pokemon(result["name"], result["id"], result["height"], result["weight"]))
+            print("33455")
+            return Pokemon(result["name"], result["id"], result["height"], result["weight"])
+
+    def get_all(self, max_counter: int, get_full=False) -> Iterator:
+        counter: int = 1
         while counter <= max_counter:
             url = "https://pokeapi.co/api/v2/pokemon/" + str(counter) + "/"
             result = requests.get(url).json()
@@ -49,12 +72,17 @@ class PokeAPI:
                 yield Pokemon(result["name"], result["id"], result["height"], result["weight"])
             counter += 1
 
-print(PokeAPI().get_pokemon("ditto"))
 
-maxx_weight = 0
-pok = ''
-for i in PokeAPI().get_all(50,True):
+pokeball = []
+maxx_weight: int = 0
+
+print(PokeAPI().get_pokemon("ditto"))
+print(PokeAPI().get_pokemon("ditto"))
+#print(PokeAPI().get_pokemon("110"))
+"""
+for i in PokeAPI().get_all(50, True):
     if maxx_weight < i.weight:
         maxx_weight = i.weight
         pok = i
 print(pok)
+"""
